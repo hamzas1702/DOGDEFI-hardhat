6375cd0976ef7be6d8efc6bf68048496488bc6a9
jahadtariq <muhammadjahadt@gmail.com>
2024-10-02 01:42:28 +0500

    created git repo for hardhat

---
+++ deploy.js
@@ -0,0 +1,22 @@
+const hre = require("hardhat");
+
+async function main() {
+    // Get the contract factory for TokenFactory
+    const TokenFactory = await hre.ethers.getContractFactory("TokenFactory");
+
+    // Deploy the contract
+    const tokenFactory = await TokenFactory.deploy();
+
+    // Wait for the deployment to finish
+    await tokenFactory.deploymentTransaction().wait();
+
+    // Output the deployed contract address
+    console.log("TokenFactory deployed to:", tokenFactory.target);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      chainId: 1,
+      // blockGasLimit: 12000000, // Custom gas limit
+      // gasPrice: 20000000000, // Custom gas price (in wei)
+      // initialBaseFeePerGas: 0, // Base fee per gas for the EIP-1559 (use 0 for legacy-style gas pricing)
+      // mining: {
+      //   auto: true, // Enable automatic mining
+      //   interval: 5000, // Optional: Manual mining interval (in ms)
+      // },
+      forking: {
+        url: "https://site1.moralis-nodes.com/eth/4439963949d341ca8f99cab64b8dab55",
+      },
+    }
+  }
+};
